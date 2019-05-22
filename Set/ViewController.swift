bd2c079fec50f90f3edbe8ee227ce9f9ca4a19d7
Russell Snyder <russell@rustybass.com>
2019-05-22 14:38:32 +0200

    Initial Commit

---
+++ Set/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Set
+//
+//  Created by Russell Wunder on 22.05.19.
+//  Copyright © 2019 Russell Snyder. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
